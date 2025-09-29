47d05c0ad84553deff99cd84f1ec674593d5abf5
LFMG1993 <lfmontes662@soy.sena.edu.co>
2025-09-28 21:33:21 -0500

    ajustes de idioma

--- src/hooks/i18n.ts
+++ src/hooks/i18n.ts
@@ -1,5 +1,5 @@
 import i18n from "i18next";
-import { initReactI18next } from "react-i18next";
+import {initReactI18next} from "react-i18next";
 import LanguageDetector from 'i18next-browser-languagedetector';
 import HttpApi from 'i18next-http-backend';
 
@@ -11,13 +11,23 @@ i18n
     // Pasa la instancia de i18n a react-i18next
     .use(initReactI18next)
     .init({
+        // --- CONFIGURACIÓN PRINCIPAL ---
+        lng: "es",
         fallbackLng: "es",
         supportedLngs: ['es', 'en'],
+        debug: process.env.NODE_ENV === 'development', // Muestra logs solo en desarrollo
+
+        interpolation: {
+            escapeValue: false, // React ya se encarga de la seguridad (XSS)
+        },
+
         // Configuración para el detector de idioma
         detection: {
-            order: ['localStorage', 'cookie', 'htmlTag', 'path', 'subdomain'],
+            // 1. Revisa localStorage, 2. Revisa el idioma del navegador
+            order: ['localStorage', 'navigator'],
             caches: ['localStorage'],
         },
+
         // Configuración para el backend que carga los JSON
         backend: {
             loadPath: '/locales/{{lng}}/translation.json',
