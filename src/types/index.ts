658322fb69280549812e80938efa05bb8260d901
LFMG1993 <lfmontes662@soy.sena.edu.co>
2025-11-20 00:06:13 -0500

    ajustes en las exportaciones de tipos innecesarias

--- src/types/index.ts
+++ src/types/index.ts
@@ -2,7 +2,6 @@ export * from './product.types.ts' ;
 export * from './user.types.ts';
 export * from './category.types.ts';
 export * from './attribute.types.ts';
-export * from './productStatic.types.ts'
 export * from './provider.types.ts'
 export * from './inventory.types.ts'
 export * from './orders.types.ts'
